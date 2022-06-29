2657bd436f8b0239f06d39dc14c6b756705cff6f
JojoKn <h12113366@s.wu.ac.at>
2022-06-29 15:38:40 +0200

    trying hard here

--- Homework R-Code.R
+++ Homework R-Code.R
@@ -43,7 +43,7 @@ wcde_data2<-wcde_data2[,c("Area", "Year", "Years", "ISOCode3")]
 
 #Selecting the (hopefully?) relevant columns 
 
-pwt101<-pwt100[,c("countrycode", "country", "year", "cgdpe", "cgdpo", "cn", "emp")]
+pwt101<-pwt100[,c("countrycode", "country", "year", "cgdpe", "cgdpo", "cn", "emp", "pop")]
 View(pwt101)
 
 #renaming columns from Wittgensteincenter
@@ -59,16 +59,64 @@ pwt101<-pwt101[pwt101$year%in%years_wcde,]
 pwt101<-dplyr::rename(pwt101, ISOCode3=countrycode)
 total<-merge(wcde_data2, pwt101, by=c("ISOCode3", "year"))
 
+#creating dummyvariables for Oil, Africa, and America
+
+oilexporters<-c("CAN", "IRQ", "KAZ", "KWT", "NGA", "NOR", "RUS", "SAU", "ARE", "USA")
+africancountries<-c("DZA", "AGO", "BEN", "BWA", "BFA", "BDI", "CPV", "CMR", "CAF", "TCD", "COM",
+                    "COD", "COG", "CIV", "DJI", "EGY", "GNQ", "ERI", "SWZ", "ETH", "GAB", "GMB",
+                    "GHA", "GIN", "GNB", "KEN", "LSO", "LBR", "LBY", "MDG", "MWI", "MLI", "MRT",
+                    "MUS", "MYT", "MAR", "MOZ", "NAM", "NER", "NGA", "REU", "RWA", "STP", "SEN",
+                    "SYC", "SLE", "SOM", "ZAF", "SSD", "SDN", "TZA", "TGO", "TUN", "UGA", "ESH",
+                    "ZMB", "ZWE")
+laamericancountries<-c("ARG", "BLZ", "BOL", "BRA", "CHL", "COL", "CRI", "ECU", "SLV", "GUF",
+                       "GTM", "GUY", "HND", "MEX", "NIC", "PAN", "PRY", "PER", "SUR", "URY", "VEN")
+
+
+total$oil<-ifelse(total$ISOCode3%in%oilexporters, "1", "0")
+total$africa<-ifelse(total$ISOCode3%in%africancountries, "1", "0")
+total$laamer<-ifelse(total$ISOCode3%in%laamericancountries, "1", "0")
+
+
 ####a####
 
-years_a<-c(1960, 1985)
+#Filter out relevant years for task a
+years_a<-c(1965, 1985)
 total_a<-total[total$year%in%years_a,]
+View(total_a)
+
+#Omit NAs
+total_a_clean<-na.omit(total_a)
+
+#Keep duplicated rows only
+total_a_clean<-total_a_clean[ ave(1:nrow(total_a_clean), 
+                                  total_a_clean$ISOCode3, FUN=length) > 1 , ]
+
+#Check: Length should be half of the number of rows from the data frame
+length(unique(total_a_clean$ISOCode3))
+
+#Calculating log differences
+
+data65<-subset(total_a_clean, year==1965)
+data85<-subset(total_a_clean, year==1985)
+
+#Get per capita income
+data65$cgdpo<-data65$cgdpo/data65$pop
+data85$cgdpo<-data85$cgdpo/data85$pop
 
 
+dy<-log(data85$cgdpo/data65$cgdpo)
+dK<-log(data85$cn/data65$cn)
+dL<-log(data85$emp/data65$emp)
+dH<-log(data85$Years/data65$Years)
+logY0<-log(data65$cgdpo)
 
+model1<-summary(lm(dY~dK+dL+dH))
 
+model2<-summary(lm(dY~dK+dL+dH+logY0))
 
+model3<-summary(lm(dY~dK+dL+dH+logY0+data65$oil))
 
+model4<-summary(lm(dY~dK+dL+dH+logY0+data65$africa+data65$laamer))
 
 
 
