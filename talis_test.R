4588c4cfcff1d8d2661b4a63a1d548abb96922f3
talistebecis <talis.tebecis@gmail.com>
2022-06-29 12:57:41 +0200

    Reworked dataset and updated code

--- talis_test.R
+++ talis_test.R
@@ -2,32 +2,32 @@
 
 library(readr)
 library(dplyr)
-pwt_all <- read_csv("pwt_all.csv", col_types = cols(country_ISO = col_number(), 
-                                                    id = col_number(), year = col_number(), 
-                                                    lnY = col_number(), lnK = col_number(), 
-                                                    lnL = col_number(), lnH = col_number(), 
-                                                    dY = col_number(), dK = col_number(), 
-                                                    dL = col_number(), dH = col_number(), 
-                                                    Y0 = col_number(), missing_data_85 = col_character()))
+pwt_all <- read_csv("pwt_all.csv", col_types = cols(year = col_number(), 
+                                                      rgdpe = col_number(), rgdpo = col_number(), 
+                                                      pop = col_number(), Y_pc = col_number(), 
+                                                      emp = col_number(), cn = col_number(), 
+                                                      H = col_number(), DY = col_number(), 
+                                                      DK = col_number(), DL = col_number(), 
+                                                      DH = col_number(), Y0 = col_number()))
 
 #filter for 1985 and remove missing values
 data <- pwt_all %>%
-        filter(year == 1985, missing_data_85 == "N")
+        filter(year == 1985)
 
 
 #plot correlation
-plot(dY ~ dK, data = data)
-plot(dY ~ dL, data = data)
-plot(dY ~ dH, data = data)
+plot(DY ~ DK, data = data)
+plot(DY ~ DL, data = data)
+plot(DY ~ DH, data = data)
 
-model1 <- lm(dY ~ dK + dL + dH, data = data)
+model1 <- lm(DY ~ DK + DL + DH, data = data)
 summary(model1)
 
-model2 <- lm(dY ~ dK + dL + dH + Y0, data = data)
+model2 <- lm(DY ~ DK + DL + DH + Y0, data = data)
 summary(model2)
 
-model3 <- lm(dY ~ dK + dL + dH + Y0 + OIL, data = data)
+model3 <- lm(DY ~ DK + DL + DH + Y0 + OIL, data = data)
 summary(model3)
 
-model4 <- lm(dY ~ dK + dL + dH + Y0 + AFRICA + LAAMER, data = data)
+model4 <- lm(DY ~ DK + DL + DH + Y0 + AFRICA + LAAMER, data = data)
 summary(model4)
